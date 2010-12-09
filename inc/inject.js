da6ccda0b1cf3b77b4b6f78d386e2434cf5f945c
Ben Lowery <ben@blowery.org>
2010-12-09 17:08:54 -0500

    add in a wrapper for DOH-based tests. Could use a fair bit more refactoring.

--- inc/inject.js
+++ inc/inject.js
@@ -1,14 +1,16 @@
 function $yetify (config) {
 
     var w = window,
+        Y = ("Y" in w) ? w.Y : false,
         Y2 = ("YAHOO" in w) ? w.YAHOO : false,
-        YTest = w.YUITest || Y2.TestRunner,
+        DOH = ("doh" in w) ? w.doh : false,
+        TestHarness = w.YUITest || Y2.TestRunner || DOH,
         matches;
 
     // No YUI? Drop and move on.
     // This file probably 404ed.
     // TODO: Stop eating this error.
-    if (!Y2 && !w.YUI) return parent.YETI.next();
+    if (!Y2 && !w.YUI && !DOH) return parent && parent.YETI && parent.YETI.next();
 
     if (!$yetify.config) { // first run
 
@@ -36,11 +38,15 @@ function $yetify (config) {
         return; // yui2-json will call $yetify when ready
     }
 
-    // poll for Y.Test
-    if (!YTest) return w.setTimeout($yetify, 50);
+    // poll for Y.Test or DOH
+    if (!TestHarness) return w.setTimeout($yetify, 50);
+
+    var NullYeti = {
+        heartbeat: function() {}
+    };
 
     var href = w.location.href,
-        YETI = parent.YETI;
+        YETI = parent.YETI || NullYeti;
 
     YETI.heartbeat();
 
@@ -50,15 +56,113 @@ function $yetify (config) {
         // leave other versions of IE alone
         return (v == 9) ? 0 : v;
     }
+    
+    function DohTestReporter(url, format) {
+        // an adapter for the DOH test runner
+        this.url = url;
+        this.format = format;
+        this._fields = {};
+        this._form = null;
+        this._iframe = null;
+    }
+    
+    DohTestReporter.prototype = {
+        constructor: DohTestReporter,
+        
+        addField: function(name, value) {
+            this._fields[name] = value;
+        },
+        
+        clearFields: function() { 
+            this._fields = {};
+        },
+        
+        destroy: function() {
+            this._form && dojo.destroy(this._form);
+            this._iframe && dojo.destroy(this._iframe);
+            this._form = this._iframe = this._fields = null;
+        },
+        
+        report: function(results) {
+            //if the form hasn't been created yet, create it
+            if (!this._form){
+                this._form = document.createElement("form");
+                this._form.method = "post";
+                this._form.style.visibility = "hidden";
+                this._form.style.position = "absolute";
+                this._form.style.top = 0;
+                document.body.appendChild(this._form);
+            
+                //IE won't let you assign a name using the DOM, must do it the hacky way
+                if (dojo.isIE < 9){
+                    this._iframe = document.createElement("<iframe name=\"yuiTestTarget\" />");
+                } else {
+                    this._iframe = document.createElement("iframe");
+                    this._iframe.name = "yuiTestTarget";
+                }
+    
+                this._iframe.src = "javascript:false";
+                this._iframe.style.visibility = "hidden";
+                this._iframe.style.position = "absolute";
+                this._iframe.style.top = 0;
+                document.body.appendChild(this._iframe);
+    
+                this._form.target = "yuiTestTarget";
+            }
+    
+            //set the form's action
+            this._form.action = this.url;
+        
+            //remove any existing fields
+            while(this._form.hasChildNodes()){
+                this._form.removeChild(this._form.lastChild);
+            }
+            
+            //create default fields
+            this._fields.results = this.format(results);
+            this._fields.useragent = navigator.userAgent;
+            this._fields.timestamp = (new Date()).toLocaleString();
+    
+            //add fields to the form
+            for(var prop in this._fields) {
+                var value = this._fields[prop];
+                if (typeof value != "function"){
+                    var input = document.createElement("input");
+                    input.type = "hidden";
+                    input.name = prop;
+                    input.value = value;
+                    this._form.appendChild(input);
+                }
+            }
+    
+            //remove default fields
+            delete this._fields.results;
+            delete this._fields.useragent;
+            delete this._fields.timestamp;
+            
+            if (arguments[1] !== false){
+                this._form.submit();
+            }
+        
+        }
+    }
+    
+    function DohJsonFormatter(results) {
+        // assumes we have dojo available
+        return dojo.toJson(results);
+    }
 
     function attachReporter (Y) {
 
         var TestReporter, FormatJSON, self;
 
-        if (Y === Y2) { // yui 2.x
+        if (Y && Y === Y2) { // yui 2.x
             TestReporter = Y.tool.TestReporter;
             FormatJSON = Y.tool.TestFormat.JSON;
             Y.env.ua.ie = fixIE9(Y.env.ua.ie);
+        } else if(DOH) {
+            TestReporter = DohTestReporter;
+            FormatJSON = DohJsonFormatter;
         } else {
             TestReporter = Y.Test.Reporter;
             FormatJSON = Y.Test.Format.JSON;
@@ -101,8 +205,63 @@ function $yetify (config) {
         if (document.compatMode !== "CSS1Compat") {
             w.onerror("Not in Standards Mode!");
         }
+        
+        function DohRunnerAdapter() {
+            this._root = null;
+            
+            dojo.connect(doh, "_onEnd", this, "_dohComplete");
+            dojo.connect(doh, "_testFinished", this, "_dohTestFinished");
+            
+            if(doh._testCount > 0) {
+                // tests already ran
+                this._root = {
+                    results: {
+                        name: href,
+                        total: doh._testCount,
+                        passed: doh._testCount - (doh._errorCount + doh._failureCount),
+                        failed: doh._errorCount + doh._failureCount
+                    }
+                };
+            }
+        }
+        
+        DohRunnerAdapter.prototype = {
+        
+            constructor: DohRunnerAdapter,
+            
+            COMPLETE_EVENT: "onComplete",
+            TEST_PASS_EVENT: "onTestPass",
+            TEST_FAIL_EVENT: "onTestFail",
+            TEST_IGNORE_EVENT: "onTestIgnore",
+            
+            onComplete: function() {},
+            onTestPass: function() {},
+            onTestFail: function() {},
+            onTestIgnore: function() {},
+            
+            subscribe: function(eventName, fn) {
+                dojo.connect(this, eventName, fn);
+            },
+            
+            _dohComplete: function() {
+                this.onComplete({
+                    results: {
+                        name: href,
+                        total: doh._testCount,
+                        passed: doh._testCount - (doh._errorCount + doh._failureCount),
+                        failed: doh._errorCount + doh._failureCount
+                    }
+                });
+            },
+            
+            _dohTestFinished: function(group, fixture, success) {
+                if(success) this.onTestPass();
+                else this.onTestFail();
+            }
+        };
+        
 
-        var Runner = YTest.TestRunner || YTest;
+        var Runner = DOH ? new DohRunnerAdapter() : (YTest.TestRunner || YTest);
 
         if (Runner._root && Runner._root.results && Runner._root.results.type == "report") {
             return submit(Runner._root);
@@ -115,7 +274,8 @@ function $yetify (config) {
 
     }
 
-    if (Y2) attachReporter(Y2)
+    if (DOH) attachReporter()
+    else if (Y2) attachReporter(Y2)
     else w.YUI().use("test", attachReporter);
 
 };
